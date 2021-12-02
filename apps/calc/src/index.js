10039ec1e1e1ee1635301387de1921a1dc667539
Roberto Achar <robertoachar@gmail.com>
2021-12-02 15:46:46 -0300

    feature: add apps and packages

---
+++ apps/calc/src/index.js
@@ -0,0 +1,8 @@
+import { sum } from '@playground/sum';
+import { subtract } from '@playground/subtract';
+
+const totalSum = sum(5, 5);
+console.log(totalSum);
+
+const totalSubtract = subtract(5, 5);
+console.log(totalSubtract);
